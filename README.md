578078be0dd76e226042a8f95161b36ae777bd06
Rakshan <shetty.raxx555@gmail.com>
2016-07-24 10:35:45 +0530

    Project Setup

---
+++ README.md
@@ -0,0 +1,28 @@
+A static version of docstash
+=============================
+
+
+
+
+
+## System Preparation
+
+To use this project, you'll need the following things installed on your machine.
+
+1. [Ruby](https://www.ruby-lang.org/en/) - use the installer.
+2. [Jekyll](http://jekyllrb.com/) - `$ gem install jekyll`
+3. [NodeJS](http://nodejs.org) - use the installer.
+4. [GulpJS](https://github.com/gulpjs/gulp) - `$ npm install -g gulp`
+
+## Local Installation
+
+1. Clone this repo, or download it into a directory of your choice.
+2. Inside the directory, run `npm install`.
+
+## Usage
+
+
+
+```shell
+$ npm start
+```
---
+++ gulpfile.js
@@ -0,0 +1,90 @@
+const gulp        = require('gulp');
+const browserSync = require('browser-sync');
+const sass        = require('gulp-sass');
+const prefix      = require('gulp-autoprefixer');
+const cp          = require('child_process');
+const cleanCSS    = require('gulp-clean-css');
+const uglify      = require('gulp-uglify');
+const htmlmin     = require('gulp-htmlmin');
+const imagemin    = require('gulp-imagemin');
+const plumber = require('gulp-plumber');
+
+var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+var messages = {
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
+
+/**
+ * Build the Jekyll Site
+ */
+gulp.task('jekyll-build', function (done) {
+    browserSync.notify(messages.jekyllBuild);
+    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
+        .on('close', done);
+});
+
+/**
+ * Rebuild Jekyll & do page reload
+ */
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
+});
+
+/**
+ * Wait for jekyll-build, then launch the Server
+ */
+gulp.task('browser-sync', ['sass', 'jekyll-build','js'], function() {
+    browserSync({
+        server: {
+            baseDir: '_site'
+        }
+    });
+});
+
+/**
+ * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
+ */
+gulp.task('sass', function () {
+    return gulp.src('assets/css/main.sass')
+        .pipe(plumber())
+        .pipe(sass({
+            includePaths: ['css'],
+            onError: browserSync.notify
+        }))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('_site/assets/css'))
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('js', function() {
+  return gulp.src('assets/js/main.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('assets/js/min/'));
+});
+gulp.task('html', function() {
+  return gulp.src('_site/**/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('_site'))
+});
+gulp.task('image', () =>
+    gulp.src('assets/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('assets/img/'))
+);
+/**
+ * Watch scss files for changes & recompile
+ * Watch html/md files, run jekyll & reload BrowserSync
+ */
+gulp.task('watch', function () {
+    gulp.watch('assets/css/**/*.scss', ['sass']);
+    gulp.watch('assets/js/**', ['js','jekyll-rebuild']);
+    gulp.watch(['*.html', '_layouts/*.html', '_includes/*.html'], ['jekyll-rebuild']);
+});
+
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the jekyll site, launch BrowserSync & watch files.
+ */
+gulp.task('default', ['browser-sync', 'watch']);
